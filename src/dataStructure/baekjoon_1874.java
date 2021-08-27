6ec6d34711d96f40c2734b279825f86cb7c994c1
stillgood96 <a787574@naver.com>
2021-08-28 00:00:24 +0900

    1874

---
+++ src/dataStructure/baekjoon_1874.java
@@ -0,0 +1,42 @@
+package dataStructure;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class baekjoon_1874 {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
+
+        int count = sc.nextInt();
+
+        int max = 0;
+
+        while(count-- >0) {
+
+            int val = sc.nextInt();
+
+            if(val > max) {
+
+                for(int i = max +1; i<=val; i++) {
+                    stack.push(i);
+                    sb.append('+').append('\n');
+                }
+                max = val;
+            }else if(stack.peek() != val) {
+                System.out.println("NO");
+                return;
+            }
+            stack.pop();
+            sb.append('-').append('\n');
+
+        }
+        System.out.println(sb);
+
+    }
+}
