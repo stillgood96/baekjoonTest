73d080416d7e2c4fefd81923a7946109a86b0bb3
stillgood96 <a787574@naver.com>
2021-08-27 18:59:24 +0900

    백준 1001번

---
+++ src/math_10000.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class math_10000 {
+    public static void main(String[] args) {
+
+        int a,b,c;
+
+        Scanner sc= new Scanner(System.in);
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c= a+b;
+
+        System.out.println(c);
+    }
+
+}
