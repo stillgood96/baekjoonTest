694742b41d3af71aa9341043a16cc76a57bdc3ec
a7875 <a787574@naver.com>
2021-11-17 01:40:38 +0900

    Bronze V

---
+++ src/bronzeV/BaekJoon_1271.java
@@ -0,0 +1,19 @@
+package bronzeV;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BaekJoon_1271 {
+
+    //https://www.acmicpc.net/problem/1271
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        sc.close();
+        System.out.println(a.divide(b));
+        System.out.println(a.remainder(b));
+    }
+}
