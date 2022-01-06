1cbc4d89540e8b1f10895a2f90b09c471f0be6e7
stillgood96 <a787574@naver.com>
2022-01-07 01:40:08 +0900

    Bronz5 10171

---
+++ src/bronzeV/BaekJoon_10171.java
@@ -0,0 +1,14 @@
+package bronzeV;
+
+public class BaekJoon_10171 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|\n");
+        System.out.println(sb);
+
+
+    }
+}
