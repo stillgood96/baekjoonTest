eb51c79409432e32019fbb2de0386cd72bf2ff26
a7875 <a787574@naver.com>
2022-01-09 05:06:19 +0900

    입출력 단계1 - 10430

---
+++ src/sector01/BaekJoon_10430.java
@@ -0,0 +1,23 @@
+package sector01;
+
+import java.util.Scanner;
+
+public class BaekJoon_10430 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        // 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+
+    }
+}
