6fefb2f7a53e274736a89e37837972bb479f98bb
a7875 <a787574@naver.com>
2022-01-09 04:52:36 +0900

    입출력 단계1 - 1008

---
+++ src/sector01/BaekJoon_1008.java
@@ -0,0 +1,17 @@
+package sector01;
+
+import java.util.Scanner;
+
+public class BaekJoon_1008 {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.println(a / b);
+
+    }
+}
