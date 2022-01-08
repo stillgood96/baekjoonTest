55378c86c9eee3490fb99e7f642a2a42707df2ae
a7875 <a787574@naver.com>
2022-01-09 04:47:11 +0900

    입출력 단계1 - 10172

---
+++ src/sector01/BaekJoon_10172.java
@@ -0,0 +1,17 @@
+package sector01;
+
+public class BaekJoon_10172 {
+
+    public static void main(String[] args) {
+
+        // 백슬래쉬를 표현하려면 \\ 두번 적어줘야 함. ㅇㅋ?
+        // 큰따옴표는 \"
+
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
