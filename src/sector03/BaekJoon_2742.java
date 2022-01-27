e10b81b28ebc912c09a7ec3ad2e45beefd1d9875
imlsw96 <a787574@naver.com>
2022-01-27 20:22:09 +0900

    sector03 - 2742

---
+++ src/sector03/BaekJoon_2742.java
@@ -0,0 +1,16 @@
+package sector03;
+
+import java.util.Scanner;
+
+public class BaekJoon_2742 {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+
+        int test = sc.nextInt();
+
+        for(int i = test; i>0; i--) {
+            System.out.println(i);
+        }
+    }
+}
