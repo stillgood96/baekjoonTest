87311f7c30dab92966008a1f3e9ce09abe4fd6d6
imlsw96 <a787574@naver.com>
2022-01-27 20:16:34 +0900

    sector01 - 18108

---
+++ src/sector01/BaekJoon_18108.java
@@ -0,0 +1,14 @@
+package sector01;
+
+import java.util.Scanner;
+
+public class BaekJoon_18108 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int test = sc.nextInt();
+
+        System.out.println(test - 544);
+    }
+}
