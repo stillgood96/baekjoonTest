87040df274bbaacd4f1dc0da69bd54e765c83467
imlsw96 <a787574@naver.com>
2022-03-18 00:04:18 +0900

    Sector05 - 8958

--- src/sector05/BaekJoon_8958.java
+++ src/sector05/BaekJoon_8958.java
@@ -6,24 +6,28 @@ import java.util.Scanner;
 
 public class BaekJoon_8958 {
 
-    public static void main(String[] args) throws Exception {
-        // TODO Auto-generated method stub
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int score, continuity;
-        String temp;
-        for(int i=0;i<n;i++) {
-            score=continuity=0;
-            temp = br.readLine();
-            for(int j=0;j<temp.length();j++) {
-                if(temp.charAt(j)=='O') {
-                    continuity++;
-                    score+=continuity;
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int count = Integer.parseInt(sc.nextLine());
+
+        for(int i=0; i< count; i++) {
+
+            int startScore = 0;
+            int totalScore = 0;
+
+            String val = sc.nextLine();
+
+            for(int j = 0; j < val.length(); j++) {
+                if(val.charAt(j) == 'O') {
+                    startScore++;
+                    totalScore += startScore;
+                }else {
+                    startScore = 0;
                 }
-                else
-                    continuity=0;
             }
-            System.out.println(score);
+            System.out.println(totalScore);
         }
     }
 }
