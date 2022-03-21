1c39319a860f32b1b2bae46bedb38fd35df6a4e2
imlsw96 <a787574@naver.com>
2022-03-21 12:37:20 +0900

    Sector06 - 15596

---
+++ src/sector06/BaekJoon_15596.java
@@ -0,0 +1,17 @@
+package sector06;
+
+public class BaekJoon_15596 {
+
+    public int sum(int [] array) {
+        int result = 0;
+
+        for(int i =0 ; i< array.length; i++) {
+
+            result += array[i];
+        }
+
+
+
+        return result;
+    }
+}
